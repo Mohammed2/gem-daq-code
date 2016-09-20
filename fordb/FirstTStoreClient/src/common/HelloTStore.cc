b9601bd834420f910b9ee5d7182e17a80f0a55c7
castaned <amch031182@gmail.com>
2016-09-20 02:58:43 -0700

    updated

--- fordb/FirstTStoreClient/src/common/HelloTStore.cc
+++ fordb/FirstTStoreClient/src/common/HelloTStore.cc
@@ -80,9 +80,8 @@ void HelloTStore::query(xgi::Input * in, xgi::Output * out ) throw (xgi::excepti
 	  std::string value=results.getValueAt(rowIndex,*column)->toString();
 	  LOG4CPLUS_INFO(this->getApplicationLogger(),*column+": "+value);
 	  *out<<"<tr>"<<std::endl;
-	  *out<<"<th>Column  "<<*column<<"</th>"<<std::endl;
+	  *out<<"<td>"<<*column<<"  :"<<"  Value  "<<value<"</td>"<<std::endl;
 	  *out<<"</tr>"<<std::endl;
-	  //	  *out<<"<td>   Value  "<<value<<"</td>"<<std::endl;
 	}
       }
     }
