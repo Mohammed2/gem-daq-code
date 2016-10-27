1dacdbf649c4cd9b8b59d897626d98bf98b2a83d
castaned <amch031182@gmail.com>
2016-10-27 14:04:51 +0300

    updated

---
+++ fordb/FirstTStoreClient/include/SimpleWeb.h
@@ -0,0 +1,14 @@
+#ifndef SimpleWeb_h
+#define SimpleWeb_h
+#include "xdaq/WebApplication.h"
+#include "xgi/Method.h"
+#include "cgicc/HTMLClasses.h"
+class SimpleWeb: public xdaq::Application {
+        public:
+        
+        XDAQ_INSTANTIATOR();
+        
+        SimpleWeb(xdaq::ApplicationStub * s) throw (xdaq::exception::Exception);
+        void Default(xgi::Input * in, xgi::Output * out ) throw (xgi::exception::Exception);
+};
+#endif
