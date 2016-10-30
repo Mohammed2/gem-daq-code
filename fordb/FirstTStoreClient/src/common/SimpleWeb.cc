c47aa37f225e53edfe5b0e70a071c081304ac1f7
castaned <amch031182@gmail.com>
2016-10-30 11:29:06 +0300

    updated

--- fordb/FirstTStoreClient/src/common/SimpleWeb.cc
+++ fordb/FirstTStoreClient/src/common/SimpleWeb.cc
@@ -62,7 +62,7 @@ SimpleWeb::getDBInfo::getDBInfo(){
   
   xoap::MessageReference ViewInfo = GEMDBobj.getViewInfo("VFAT2");
 
-  // std::string connectionID = GEMDBobj.connect(sendSOAPMessage(ViewInfo));
+  std::string connectionID = GEMDBobj.connect(SimpleWeb::sendSOAPMessage(ViewInfo));
 
   // xdata::Table results;
 
